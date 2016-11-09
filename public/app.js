d5e8789daef2776eb6c4fa990b7c2811d3f8aca8
muriel <murielthoms@gmail.com>
2016-11-09 23:44:30 +0100

    mini crm fonctionnel, manque bug crm.json

--- public/app.js
+++ public/app.js
@@ -1,35 +1,53 @@
 (function(){
 	"use.strict";
 	var app = {
+
 		init: function(){
-
-			this.recupDonnees();
-		},
-		
-		recupDonnees: function(){
-			$.ajax('/crm.json')
-			.done(this.success)
-			.fail(this.echec);
+			this.ajaxDonnees();
+			$('form').on('submit',this.handleForm.bind(this));
 		},
 
-		success: function(data){
-			console.log(data.customers[0].id);
+		ajaxDonnees: function(){
+			$.ajax('/crm.json').done(this.afficheDonnees);
+		},
+
+		afficheDonnees: function(data){
 			var donnees = data.customers.length;
 			for(var i=0; i<donnees; i++){
 				var obj = data.customers[i];
 				for(prop in obj){
 					var coordonnees= prop + " : " + obj[prop];
-					$('#informations').append('<ul><li>' + coordonnees + '</li></ul>');
-
+					$('#informations').append('<ul>' + coordonnees + '</ul>');
 				}
 			}
 		},
 
+		handleForm: function(event){
+			event.preventDefault();
+			var nom = $('#nom').val();
+			var prenom = $('#prenom').val();
+			var phone = $('#phone').val();
+			var mail = $('#mail').val();
+			var description = $('#description').val();
+			this.submitForm({nom:nom, prenom:prenom, phone:phone, mail:mail, description:description});
+		},
 
+		submitForm: function(data){
+			$.ajax({
+				type:"POST",
+				url:$("form").attr("action"),
+				data: data,
+				success : this.success
+			})
+		}, 
 
+		success: function(){
+			console.log('very good!');
+		}	
 	};
+
 	$(document).ready(function(){
 
-		app.init();  
+		// app.init();  
 	});
 }());
---
+++ server.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var fs = require('fs');
+
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+// app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser());
+
+
+app.post('/users', function(req, res){
+	
+// fs = interagir dossier système; 
+// 1 argument:nom du dossier 2 argument:callback;
+// le callback prend 2 fonctions: err,data.
+fs.readFileSync('public/crm.json', 'utf8',function(err,data){
+	if (err){
+		return console.log('fichier non trouvé');
+	}
+		// je transforme chaine de caractère en objet;
+		var body = req.body;
+		console.log(body);
+
+		// contient le nombre d'éléments de l'objet;
+		body.id = body.customers.length+1;
+		// ajout des nouveaux éléments dans l'objet
+		data.customers.push(body);
+		// je retransforme l'objet en chaine de caractère
+		var jsonObj = JSON.stringify(data);
+
+		// ecrire  fichier modifié (nomDuFichier, contenu, erreur)
+		fs.writeFile('public/crm.json',jsonObj,'utf8', function(err){
+			if(err){
+				return	console.log(err);
+
+			}
+
+		})
+	});
+res.json('envoi réussi!')
+
+});
+app.listen(2345, function(){
+	console.log(' ça fonctionne...ou pas!');
+
+});
